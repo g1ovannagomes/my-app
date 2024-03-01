4f5dd3a1194ac0cca29cccefbfe181b66817a2a9
GIOVANA ISABELI GOMES FERREIRA <giovana.ferreira68>
2024-03-01 11:42:54 -0300

    reset css e link

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function sobre () {
+    return(
+        <>
+        <h1>sobre</h1>
+        <p>tela sobre meu projeto</p>
+        <Link href={"/"}>Home</Link>
+        </>
+    );
+}
